6a23ae441e1370168364f78a1727fa5cb8134dd8
Justin Teeter <jteeter2007@gmail.com>
2018-11-21 12:04:52 -0800

    Create main.java

---
+++ main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+import java.util.Scanner;
+import java.util.Vector;
+/**
+ *
+ * @author ncc
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Vector <Painting> p;
+        Vector <Sculpture> s;
+         
+          
+        
+        
+        
+    }
+    
+    public void menu() {
+            
+    }
+    
+}
